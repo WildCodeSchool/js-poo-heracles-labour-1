feb98202e18140e39e8a579e57a9361781ed3893
nitramXmas <24914535+nitramXmas@users.noreply.github.com>
2023-03-25 18:20:17 +0100

    fight is on, to do : minus valor bug and final prompt

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+import {Fighter } from "./src/Fighter.js";
+
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+
+const heracles = new Fighter("🧔 Heracles", 20, 6);
+const lion = new Fighter("🦁 Nemean Lion", 11, 13);
+let round = 1;
+while(heracles.life >= 0 || lion.life >= 0) {
+    console.log(`Round: ${round}`);
+    heracles.fight(lion);
+    console.log(`${heracles.name}   🗡️    ${lion.name}. ❤️   of ${lion.name}: ${lion.life}`)
+    lion.fight(heracles);
+    console.log(`${lion.name}   🐾    ${heracles.name}. ❤️   of ${heracles.name}: ${heracles.life}`)
+    round ++;
+}
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,18 @@
 /* Fighter class definition */
+const MAX_LIFE = 100;
+class Fighter {
+    constructor (name, str, dex) {
+        this.name = name;
+        this.str = str;
+        this.dex = dex;
+        this.life = MAX_LIFE;
+    }
+
+    fight(defender){
+        let damage = Math.abs((1 + Math.floor(Math.random() * defender.str)) - defender.dex)
+        console.log(damage)
+        defender.life -= damage
+    }
+}
+
+export {Fighter}
