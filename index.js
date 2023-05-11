3aa34d8583206fb3c2650de019b94f4046de1bc3
WALLET Christophe (SNCF VOYAGEURS / DIRECTION DE LIGNES CNU / PROD_PCAT_TN) <8106550t@commun.ad.sncf.fr>
2023-05-11 12:27:26 +0200

    ajout classe et method fight

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+const Fighter = require('./src/Fighter');
+const heracles = new Fighter('🧔 Heracles', 50, 6);
+const nemeanLion = new Fighter('🦁 Nemean Lion',11 , 13);
+
+
+
+console.log(heracles.fight(nemeanLion));
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,29 @@
-/* Fighter class definition */
+const max_life = 100;
+
+class Fighter { 
+    constructor(name, strength, dexterity) {
+        this.name = name;
+        this.strength = strength;
+        this.dexterity = dexterity;
+        this.life = max_life;
+    }
+
+fight(defender) {   
+    let damage = Math.round(Math.random()* this.strength) - defender.dexterity ;
+    console.log(damage);
+    if (damage < 0) {
+        damage = 0;
+    }
+    if (defender.life - damage < 0) {
+        defender.life = 0;
+    }else {
+        defender.life -= damage;
+    }
+    console.log(defender.life);
+
+    return `${this.name} frappe ${defender.name} et lui inflige ${damage} de dégats`;
+}}
+
+
+
+module.exports = Fighter;
